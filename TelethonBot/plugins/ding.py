aabc858dabf4718c5d9491ebac13d551cb65085b
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 17:03:50 +0530

    Create ding.py

---
+++ TelethonBot/plugins/ding.py
@@ -0,0 +1,29 @@
+import asyncio
+from asyncio import wait
+from telethon import events
+from .. import BotzHub
+
+
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/ding"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.5
+    animation_ttl = range(0, 10)
+    await event.edit("DoNG!")
+    animation_chars = [
+        "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+        "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+        "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+        "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+        "⬜⬜⬛⬛🔴\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+        "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+        "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+        "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+        "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+        "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n⬜  [BECOME A VIDHAYAK](https://github.com/indianbhaiya/IndianBot/) ⬜\n⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i])
+        i += 1
