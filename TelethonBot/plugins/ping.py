7ead065165d425b80464bb405c916cce7d9f0172
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 17:33:26 +0530

    Update ping.py

--- TelethonBot/plugins/ping.py
+++ TelethonBot/plugins/ping.py
@@ -5,7 +5,8 @@ from asyncio import wait
 from telethon import events
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/ping"))
-async def ping (e):
+async def ping (e):    
+    if not e.fwd[0].isalpha() and e.fwd[0] not in ("#", "@", "!"):
     if e.fwd_from:
         return
     start = datetime.now()
