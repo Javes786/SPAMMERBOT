8a7dd8a7dcb6f6538c54ac21531e306cf19a4a82
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 13:52:27 +0530

    Create bigspam.py

---
+++ TelethonBot/plugins/bigspam.py
@@ -0,0 +1,28 @@
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/bigspam"))
+async def bigspam(e):
+
+    if not e.text[0].isalpha() and e.text[0] not in (".", "#", "@", "!"):
+
+        message = e.text
+
+        counter = int(message[9:13])
+
+        spam_message = str(e.text[13:])
+
+        for i in range(1, counter):
+
+            await e.respond(spam_message)
+
+        await e.delete()
+
+        if LOGGER:
+
+            await e.client.send_message(
+
+                LOGGER_GROUP,
+
+                "#BIGSPAM \n\n"
+
+                "Bigspam was executed successfully"
+
+                )
