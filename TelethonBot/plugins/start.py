384ad6e2fd9d276c238b239a65b5d49005401480
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 14:59:55 +0530

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -5,12 +5,12 @@ from telethon import events, Button
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("Hello!",
+    await event.reply("Some iNFoRMaTioN AbouT Me!",
                     buttons=[
-                        [Button.url("ButtonUrl", url="https://t.me/xditya")],
-                        [Button.inline("Inline Button",data="example")]
+                        [Button.url("OWNER", url="https://t.me/CRiMiNaL786")],
+                        [Button.inline("Commands",data="1")]
                     ])
 
-@BotzHub.on(events.callbackquery.CallbackQuery(data="example"))
+@BotzHub.on(events.callbackquery.CallbackQuery(data="1"))
 async def ex(event):
-    await event.edit("You clicked a button!")
+    await event.edit("/bigspam (Number of spam) (spamming text) ")
