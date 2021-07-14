0b96c6ca26085e91dc6be040a476db76bb9dfa10
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 17:02:20 +0530

    Create ping.py

---
+++ TelethonBot/plugins/ping.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from .. import BotzHub
+
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/ping"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("Pong! LauDe\n{}".format(ms))
