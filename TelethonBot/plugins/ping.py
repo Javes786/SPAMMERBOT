23b37c9e654a804b0de1bbc3d56a5c9f6a1ee6e1
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 21:39:12 +0530

    Update ping.py

--- TelethonBot/plugins/ping.py
+++ TelethonBot/plugins/ping.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import time
+import date
 from .. import BotzHub
 import asyncio
 from asyncio import wait
@@ -6,11 +7,10 @@ from telethon import events
 from . import *
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/ping"))
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    await event.edit("Pong!")
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    await event.edit("Pong! LawDe\n{}".format(ms))
+    async def handler(event):
+        s = time.time()
+        message = await event.reply('Pong!')
+        d = time.time() - s
+        await message.edit(f'Pong Lawde! __(reply took {d:.2f}s)__')
+        await asyncio.sleep(5)
+        await asyncio.wait([event.delete(), message.delete()])
