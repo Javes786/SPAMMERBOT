1deb36651f4cd14ee2b2365728498dc5ce54cef1
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 22:56:02 +0530

    Update bigspam.py

--- TelethonBot/plugins/bigspam.py
+++ TelethonBot/plugins/bigspam.py
@@ -23,3 +23,16 @@ async def bigspam(e):
                 "#BIGSPAM \n\n"
                 "Bigspam was executed successfully"
                 )
+
+@BotzHub.on(events.NewMessage(incoming=True, pattern="/spam"))
+async def spammer(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
+        message = e.text
+        counter = int(message[6:8])
+        spam_message = str(e.text[8:])
+        await asyncio.wait([e.respond(spam_message) for i in range(counter)])
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP, "#SPAM \n\n" "Spam was executed successfully"
+            )
