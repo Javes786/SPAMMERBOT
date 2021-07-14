5476f29259bd78becdae4f34f5b4a2978e1751ae
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 14:53:23 +0530

    Update bigspam.py

--- TelethonBot/plugins/bigspam.py
+++ TelethonBot/plugins/bigspam.py
@@ -6,29 +6,16 @@ from telethon import events
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
-
-    if not e.text[0].isalpha():
-
+    if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
         message = e.text
-
         counter = int(message[9:13])
-
         spam_message = str(e.text[13:])
-
         for i in range(1, counter):
-
             await e.respond(spam_message)
-
         await e.delete()
-
         if LOGGER:
-
             await e.client.send_message(
-
                 LOGGER_GROUP,
-
                 "#BIGSPAM \n\n"
-
                 "Bigspam was executed successfully"
-
                 )
