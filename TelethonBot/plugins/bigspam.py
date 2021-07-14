73a7a50a033ec3cf8b96029575f53264b6cf161a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 22:53:16 +0530

    Update bigspam.py

--- TelethonBot/plugins/bigspam.py
+++ TelethonBot/plugins/bigspam.py
@@ -12,8 +12,8 @@ LOGGER_GROUP = os.environ.get("LOGGER_GROUP", -1001547166512)
 async def bigspam(e):
     if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
         message = e.text
-        counter = int(message[9:20])
-        spam_message = str(e.text[20:])
+        counter = int(message[9:13])
+        spam_message = str(e.text[13:])
         for i in range(1, counter):
             await e.respond(spam_message)
         await e.delete()
