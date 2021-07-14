9c9c58a72934ca331dde37b16ccfb31dfe7a197c
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 18:23:08 +0530

    Update help.py

--- TelethonBot/plugins/help.py
+++ TelethonBot/plugins/help.py
@@ -14,7 +14,7 @@ async def helper(event):
     text="I am SPAMMER BOT!\nI can SPAM for you.\n\nTry Me!! Make sure to join my channel and support me!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("Channel", url=ltc), Button.url("Channel", url="https://t.me/Javes2Support")],
+                         [Button.url("Channel", url="https://t.me/Javes2Support")],
                          [Button.url("Repository", url="https://github.com/Javes786")],
                          [Button.inline("Fuck", data="Fucking You")]
                      ])
