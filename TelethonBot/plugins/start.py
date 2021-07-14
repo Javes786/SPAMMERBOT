b7bd52f706ee28f0f9325657ad78cb7e57d195da
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-14 18:06:19 +0530

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -14,3 +14,15 @@ async def start(event):
 @BotzHub.on(events.callbackquery.CallbackQuery(data="1"))
 async def ex(event):
     await event.edit("/bigspam (Number of spam) (spamming text) ")
+
+
+
+@BotzHub.on(events.callbackquery.CallbackQuery(data="helpme"))
+async def helper(event):
+    text="I am SPAMMER BOT!\nI can SPAM for you.\n\nTry Me!! Make sure to join my channel and support me!"
+    await event.edit(text,
+                     buttons=[
+                         [Button.url("Channel", url=ltc), Button.url("Dev", url="https://t.me/Javes2Support")],
+                         [Button.url("Repository", url="https://github.com/Javes786")],
+                         [Button.inline("Fuck", data="Fucking You")]
+                     ])
