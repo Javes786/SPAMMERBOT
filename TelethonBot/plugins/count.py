52082ab025c3a66d2681844020c897435ea065db
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-15 23:21:52 +0530

    Update count.py

--- TelethonBot/plugins/count.py
+++ TelethonBot/plugins/count.py
@@ -13,7 +13,7 @@ from telethon import TelegramClient as BotzHub
 
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/count"))
-async def (count):    
+async def _(count):    
     u = 0
     g = 0
     c = 0
