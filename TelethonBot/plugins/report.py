5b8d8f3569f574ff76a788638d7dfa7fc602e103
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-15 13:48:32 +0530

    Update report.py

--- TelethonBot/plugins/report.py
+++ TelethonBot/plugins/report.py
@@ -3,16 +3,16 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon import TelegramClient as  BotzHub
 from .. import BotzHub 
-from telethon import bot 
-#needed for bot 
+
+#needed for bot = client
 
 @BotzHub.on(events.NewMessage(incoming=True, pattern="/report"))
-async def _(event):
+async def report(event):
     if event.fwd_from:
         return
     mentions = "RePorted to Lawde @admin"
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
+    async for x in BotzHub.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f"[\u2063](tg://user?id={x.id})"
     reply_message = None
     if event.reply_to_msg_id:
