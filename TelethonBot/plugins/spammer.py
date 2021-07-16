cb68f490a6b20ddcf3a1bfb819d30bf02d74966a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-16 21:47:23 +0530

    Update spammer.py

--- TelethonBot/plugins/spammer.py
+++ TelethonBot/plugins/spammer.py
@@ -2,14 +2,18 @@ import asyncio
 import os
 from .. import ATGK
 from asyncio import wait
-
+from telethon.events import ChatAction
 from telethon import events
 
 LOGGER = os.environ.get("LOGGER", -1001547166512)
 LOGGER_GROUP = os.environ.get("LOGGER_GROUP", -1001547166512)
+BOT_USER = os.environ.get("BOT_USER", 511112479)
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
+    MYID = [BOT_USER]
+ if not sender.id in MYID :
+      return await event.send("U Are Not MY Owner")
     if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
         message = e.text
         counter = int(message[9:13])
