e80462a7e57b26b7be250359f0ea0c4e6508e78f
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-17 17:41:59 +0530

    Update spammer.py

--- TelethonBot/plugins/spammer.py
+++ TelethonBot/plugins/spammer.py
@@ -11,7 +11,7 @@ BOT_USER = os.environ.get("BOT_USER", 511112479)
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
-       [ BOT_USER ] = await e.get_sender
+       [ BOT_USER ] = await e.get_sender()
      if BOT_USER:
                 pass
    else:
