72613c37e51fe004e6e76fa6bcb4275097d11843
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-17 16:56:35 +0530

    Update spammer.py

--- TelethonBot/plugins/spammer.py
+++ TelethonBot/plugins/spammer.py
@@ -11,8 +11,8 @@ BOT_USER = os.environ.get("BOT_USER", 511112479)
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
-        BOT_USER = await e.get_sender_id
-       abc = await e.get_input_message()
+       await e.get_sender_id = BOT_USER
+       await e.get_input_message() = abc
   if e.abc.startswith("/bigspam") and if not BOT_USER:
        return await e.reply("`U ARE NOT MY OWNER`")
   try:
