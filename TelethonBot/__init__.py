491a701fa684429a8362ae944a9b56dd56b623dd
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-18 13:37:08 +0530

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -23,7 +23,7 @@ LOGGER = os.environ.get("LOGGER", -1001547166512)
 LOGGER_GROUP = os.environ.get("LOGGER_GROUP", -1001547166512)
 BOT_USER = os.environ.get("BOT_USER")
 BOT_USERS = str(BOT_USER).split(" ", maxsplit=len(BOT_USER))
-
+ALIVE_NAME = os.environ.get("ALIVE_NAME", "HACKER PREM")
 
 
 ATGK = TelegramClient('ATGK', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
