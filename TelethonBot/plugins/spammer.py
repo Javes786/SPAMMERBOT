9405ae3f80fbf2387419a22d003417ac86125055
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-18 08:38:05 +0530

    fixed

--- TelethonBot/plugins/spammer.py
+++ TelethonBot/plugins/spammer.py
@@ -12,10 +12,10 @@ BOT_USER = os.environ.get("BOT_USER", 511112479)
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
-                     users = [BOT_USER]
-if not e.sender_id in users:
-  return await e.reply("kid you are not my owner (sed)", alert=True)
-if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
+  users = BOT_USER
+  if not e.sender_id in users:
+    return await e.reply("kid you are not my owner (sed)")
+  if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
         message = e.text
         counter = int(message[9:13])
         spam_message = str(e.text[13:])
