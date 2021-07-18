67cb8aba0d327422e1cef9a86c40b5b632a7c517
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-07-18 14:19:20 +0530

    Update help.py

--- TelethonBot/plugins/help.py
+++ TelethonBot/plugins/help.py
@@ -10,16 +10,17 @@ from telethon import events, Button
 async def start(event):
     await event.reply("SPAMMER BOT FOR @HACKERPREM MADE BY @CRiMiNaL786",
                     buttons=[
-                        [Button.inline("Click HeRe",data="helpme")]
+                        [Button.inline("Check Me",data="helpme")]
                     ])
 
 @ATGK.on(events.callbackquery.CallbackQuery(data="helpme"))
 async def helper(event):
-    text="I am SPAMMER BOT!\nI can SPAM for you.\n\nTry Me!! Make sure to join my channel and support me!"
+    text="I Am SPAMMER BOT!\nI SPAM FOR MY MASTER.\n\nTry Me!! Request Code in Support Group!"
     await event.edit(text,
                      buttons=[
                          [Button.url("Channel", url="https://t.me/Javes2Support")],
-                         [Button.url("Repository", url="https://github.com/Javes786")],
+                         [Button.url("Group", url="https://t.me/JavesGRouP")],
+                         [Button.url("Give A STAR", url="https://github.com/Javes786")],
                          [Button.inline("Fuck", data="2")]
                      ])
 
