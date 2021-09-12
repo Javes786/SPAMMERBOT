e1b1a2cdf35c6d326654fb55b56f2d0df64d03c8
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-12 21:50:07 +0530

    Update help.py

--- TelethonBot/plugins/help.py
+++ TelethonBot/plugins/help.py
@@ -6,6 +6,9 @@ from . import *
 from .. import ATGK, ALIVE_NAME
 from telethon import events, Button
 
+
+ALIVE_NAME = str(ALIVE_NAME) if ALIVE_NAME else "SPAMMER BOT"
+
 @ATGK.on(events.NewMessage(incoming=True, pattern="/help"))
 async def start(event):
     await event.reply("SPAMMER BOT FOR {ALIVE_NAME} MADE BY @CRiMiNaL786",
@@ -18,12 +21,15 @@ async def helper(event):
     text="I Am SPAMMER BOT!\nI SPAM FOR MY MASTER.\n\nTry Me!! Request Code in Support Group!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("Channel", url="https://t.me/Javes2Support")],
-                         [Button.url("Group", url="https://t.me/JavesGRouP")],
-                         [Button.url("Give A STAR", url="https://github.com/Javes786")],
+                         [Button.url("CHANNEL", url="https://t.me/destroyxSupport")],
+                         [Button.url("GROUP", url="https://t.me/DesTRoYxOfficial")],
+                         [Button.url("GIVE STAR⭐", url="https://github.com/CRiMiNaL786")],
                          [Button.inline("Fuck", data="2")]
                      ])
 
 @ATGK.on(events.callbackquery.CallbackQuery(data="2"))
 async def ex(event):
-    await event.edit("SoRRY , Because There is No BacK Button Adding Soon")
+    await event.edit(text,
+                     buttons=[
+                         [Button.inline("BacK", data="helpme")]
+                     ])
