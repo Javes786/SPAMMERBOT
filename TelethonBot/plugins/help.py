b2eeb0ceae112324f4379fd8c2793f8d2fcfea63
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-13 20:27:45 +0530

    Update help.py

--- TelethonBot/plugins/help.py
+++ TelethonBot/plugins/help.py
@@ -11,25 +11,25 @@ ALIVE_NAME = str(ALIVE_NAME) if ALIVE_NAME else "SPAMMER BOT"
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/help"))
 async def start(event):
-    await event.reply("SPAMMER BOT FOR f'\n{ALIVE_NAME} 'MADE BY @CRiMiNaL786",
+    await event.reply("Sᴘᴀᴍᴍᴇʀ Bᴏᴛ Fᴏʀ f'{ALIVE_NAME}' Mᴀᴅᴇ Bʏ @CRiMiNaL786",
                     buttons=[
                         [Button.inline("Check Me",data="helpme")]
                     ])
 
 @ATGK.on(events.callbackquery.CallbackQuery(data="helpme"))
 async def helper(event):
-    text="I Am SPAMMER BOT!\nI SPAM FOR MY MASTER.\n\nTry Me!! Request Code in Support Group!"
+    text="I Aᴍ Sᴘᴀᴍᴍᴇʀ Bᴏᴛ!\nI Cᴀɴ Sᴘᴀᴍ Fᴏʀ Mʏ Mᴀsᴛᴇʀ.\n\nTʀʏ Mᴇ!! Rᴇϙᴜᴇsᴛ Cᴏᴅᴇ ɪɴ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ!"
     await event.edit(text,
                      buttons=[
-                         [Button.url("CHANNEL", url="https://t.me/destroyxSupport")],
-                         [Button.url("GROUP", url="https://t.me/DesTRoYxOfficial")],
-                         [Button.url("GIVE STAR⭐", url="https://github.com/CRiMiNaL786")],
-                         [Button.inline("Fuck", data="2")]
+                         [Button.url("ϲнαииєℓ", url="https://t.me/destroyxSupport")],
+                         [Button.url("gяουρ", url="https://t.me/DesTRoYxOfficial")],
+                         [Button.url("ɠเѵҽ α sԵαɾ ⭐", url="https://github.com/CRiMiNaL786")],
+                         [Button.inline("ƒυϲκ", data="2")]
                      ])
 
 @ATGK.on(events.callbackquery.CallbackQuery(data="2"))
 async def ex(event):
-    text2="YaHa KuCH Nhi BiRO."
+    text2="иοτнιиg нєяє ѕαя."
     await event.edit(text2,
                      buttons=[
                          [Button.inline("BacK", data="helpme")]
