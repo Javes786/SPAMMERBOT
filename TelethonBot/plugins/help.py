9ca4e62734f167f1cd7a07c58a22076021b8d95a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-13 21:22:58 +0530

    Update help.py

--- TelethonBot/plugins/help.py
+++ TelethonBot/plugins/help.py
@@ -11,7 +11,8 @@ ALIVE_NAME = str(ALIVE_NAME) if ALIVE_NAME else "SPAMMER BOT"
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/help"))
 async def start(event):
-    await event.reply("Sᴘᴀᴍᴍᴇʀ Bᴏᴛ Fᴏʀ "{ALIVE_NAME}" Mᴀᴅᴇ Bʏ @CRiMiNaL786",
+    tatti="Sᴘᴀᴍᴍᴇʀ Bᴏᴛ Fᴏʀ {ALIVE_NAME} Mᴀᴅᴇ Bʏ @CRiMiNaL786"
+    await event.reply(tatti,
                     buttons=[
                         [Button.inline("Check Me",data="helpme")]
                     ])
