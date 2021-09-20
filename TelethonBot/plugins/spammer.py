3187fedc943d0fc629b9cf7e4176982d9caf6e6b
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-20 21:58:39 +0530

    Update spammer.py

--- TelethonBot/plugins/spammer.py
+++ TelethonBot/plugins/spammer.py
@@ -12,7 +12,7 @@ king = [511112479]
 @ATGK.on(events.NewMessage(incoming=True, pattern="/bigspam"))
 async def bigspam(e):
   users = BOT_USERS
-  if not str(e.sender_id) in users or not e.sender_id in king:
+  if not str(e.sender_id) in users:
     return await e.reply("kid you are not my owner (sed)")
   if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
     await asyncio.sleep(0.01)
@@ -34,8 +34,10 @@ async def bigspam(e):
 @ATGK.on(events.NewMessage(incoming=True, pattern="/spam"))
 async def spammer(e):
   users = BOT_USERS
-  if not str(e.sender_id) in users or not e.sender_id in king:
+  if not str(e.sender_id) in users:
     return await e.reply("kid you are not my owner (sed)")
+  if str(e.sender_id) in users or in king:
+    await asyncio.sleep(0.01)
   if not e.text[0].isalpha() and e.text[0] not in ("#", "@", "!"):
     await asyncio.sleep(0.01)
   if re.search(abcd.lower(), e.text.lower()):
