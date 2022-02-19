d800296f5a8e00a47d675bda4494bbe11d2d57c3
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2022-02-20 01:05:46 +0530

    Update __init__.py

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -23,8 +23,8 @@ BOT_TOKEN = config("BOT_TOKEN", default=None)
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
 LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", None))
-BOT_USER = os.environ.get("BOT_USER")
-BOT_USERS = str(BOT_USER).split(" ", maxsplit=len(BOT_USER))
+BOT_USER = set(int(x) for x in os.environ.get("BOT_USER", "").split())
+
 ALIVE_NAME = os.environ.get("ALIVE_NAME", "DICTATOR AMAAN")
 
 if 511112479 not in OWNER_ID:
