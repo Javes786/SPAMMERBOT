960116297cf5aea14c5e08f5137328d44227b452
MOHAMMAD AMAAN <AMaaNTheGameRKinG@gmail.com>
2022-09-04 21:12:43 +0530

    Update __init__.py
    CORRECTIONS......

--- TelethonBot/__init__.py
+++ TelethonBot/__init__.py
@@ -17,25 +17,15 @@ yoo = logging.getLogger("WARNING")
 yooprint = logging.getLogger("WARNING")
 
 # Basics
+
 APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = config("HEROKU_API_KEY", None)
 LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", None))
-OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
-BOT_USER = set(int(x) for x in os.environ.get("BOT_USER", "").split())
-
+BOT_USER = os.environ.get("BOT_USER")
+BOT_USERS = str(BOT_USER).split(" ", maxsplit=len(BOT_USER))
 ALIVE_NAME = os.environ.get("ALIVE_NAME", "DICTATOR AMAAN")
 
-# SUPER USERS
-
-if 511112479 not in OWNER_ID:
-    OWNER_ID.append(511112479)
-
-# BOT USERS
-
-if 511112479 not in BOT_USER:
-    BOT_USER.append(511112479)
+# Client
 
 ATGK = TelegramClient('ATGK', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
